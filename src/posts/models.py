0703d92ad4288883f6779e0556589a0d495d289b
Mathias Wajnberg <mathias.wajnberg@fu-berlin.de>
2016-03-07 13:08:06 +0100

    now really finished video 27

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    image = models.FileField(null=True, blank=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True) #initial timestamp will be saved one time
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)  # auto_now refers to every modification, updated gets reset when Post is updated -duh
@@ -17,3 +18,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         #return "/posts/%s/" %(self.id)
         return reverse("posts:detail", kwargs={"id":self.id})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
@@ -27,14 +28,29 @@ def post_detail(request, id=None):
     return render(request, "post_detail.html", context_data)
 
 def post_list(request):
-    queryset = Post.objects.all().order_by('-updated')
+    queryset_list = Post.objects.all() #.order_by('-timestamp') ordering handled in meta class
+
+    paginator = Paginator(queryset_list, 5)
+    page_request_var= "page"
+    page = request.GET.get('page_request_var')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     if request.user.is_authenticated():
         context_data = {
             "object_list":queryset,
-            "title":"List"
+            "title":"List",
+            "page_request_var":page_request_var,
         }
 
     return render(request, "post_list.html", context_data)
+
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
