4e1f6661f8458a34fb8799feca3a9b8df52cb32b
Mathias Wajnberg <mathias.wajnberg@hu-berlin.de>
2017-02-17 21:38:11 +0100

    delete stuff

--- src/fileTree/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FiletreeConfig(AppConfig):
-    name = 'fileTree'
--- src/quick_format/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuickFormatConfig(AppConfig):
-    name = 'quick_format'
