aaabc70a52e3ddb29a4e5cfdf2d26e13f498e0b1
Mathias Wajnberg <Mathias.Wajnberg@hu-berlin.de>
2017-04-10 15:05:05 +0200

    rxncon update

--- setup.py
+++ setup.py
@@ -8,37 +8,24 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='rxncon_gui',
+    name='rxncon_workbench',
     scripts=[
         'runserver.py'
     ],
-    version='0.1',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
+    license='LGPL License', 
     description='A browser based interface for the rxncon framework.',
     long_description=README,
     url='https://www.rxncon.org/',
     author='Mathias Wajnberg',
     author_email='mathias.wajnberg@hu-berlin.de',
     keywords=['sysbio', 'signalling', 'systems biology'],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.10.4', 
-        'Intended Audience :: sys bio',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
     install_requires=[
         'django',
 	'rxncon',
 	'typecheck-decorator',
         'typing',
-        'xlrd',
     ]
 )
