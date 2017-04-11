870fda9c24863f90542ab7d8a259ee325c5b52d1
Mathias Wajnberg <Mathias.Wajnberg@hu-berlin.de>
2017-04-11 15:03:31 +0200

    egg sources, runserver

--- runserver.py
+++ runserver.py
@@ -1,11 +1,12 @@
 import os
 import webbrowser
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.rxncon_site.settings")
 
-from django.core.management import call_command
-from django.core.wsgi import get_wsgi_application 
 application = get_wsgi_application()
-webbrowser.open('http://127.0.0.1:8000/')
-call_command('runserver',  '127.0.0.1:8000')	
+webbrowser.open('http://127.0.0.1:8888/')
+call_command('runserver',  '127.0.0.1:8888')
 
