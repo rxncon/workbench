17f92bb3b6814ebb4c3dfa926b15f9ccf709809e
Mathias Wajnberg <Mathias.Wajnberg@hu-berlin.de>
2017-12-11 18:30:39 +0100

    pip update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     scripts=[
         'rxncon_workbench.py'
     ],
-    version='0.9',
+    version='0.95',
     packages=find_packages(),
     include_package_data=True,
     license='LGPL License', 
