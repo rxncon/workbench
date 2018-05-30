36d0876d4b9e50375627275bc72b027728df6494
Mathias Wajnberg <Mathias.Wajnberg@hu-berlin.de>
2018-05-30 15:49:11 +0200

    try to add example systems as installed resources

--- rxncon_workbench.py
+++ rxncon_workbench.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.rxncon_site.settings")
 
 application = get_wsgi_application()
-t = Timer(4, webbrowser.open, args=['http://127.0.0.1:8000/'], kwargs=None)
+t = Timer(3, webbrowser.open, args=['http://127.0.0.1:8000/'], kwargs=None)
 t.start()
 call_command('runserver',  '127.0.0.1:8000')
 
--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
     ],
     version='0.97',
     packages=find_packages(),
+    packages_dir={'media_cdn': 'media_cdn/'},
+    package_data={'media_cdn': ['hog-example/Table_S1_Hog1_acyclic_reconstruction.xls']},
     include_package_data=True,
     license='LGPL License', 
     description='A browser based interface for the rxncon framework.',
@@ -24,6 +26,17 @@ setup(
     keywords=['sysbio', 'signalling', 'systems biology'],
     install_requires=[
         'django==1.10.4',
-	    'rxncon==2.0b17',
-    ]
+        'rxncon==2.0b17',],
+    # data_files=[
+    #     ('media_cdn/hog-example/boolnet', ['media_cdn/hog-example/boolnet/hog-example_initial_vals.csv']),
+    #     ('media_cdn/hog-example/boolnet', ['media_cdn/hog-example/boolnet/hog-example_model.boolnet']),
+    #     ('media_cdn/hog-example/boolnet', ['media_cdn/hog-example/boolnet/hog-example_symbols.csv']),
+    #     ('media_cdn/hog-example/graphs',  ["media_cdn/hog-example/graphs/hog-example_Table_S1_Hog1_acyclic_reconstruction_reaGraph.xgmml"]),
+    #     ('media_cdn/hog-example/', ['media_cdn/hog-example/Table_S1_Hog1_acyclic_reconstruction.xls']),
+    #
+    #     ('media_cdn/hog-example-text-format/description', ['media_cdn/hog-example-text-format/description/hog-example-text-format_quick_definition.txt']),
+    #     ('media_cdn/hog-example-text-format/graphs', ['media_cdn/hog-example-text-format/graphs/hog-example-text-format_HOG Example text format_sReaGraph.xgmml']),
+    #     ('media_cdn/hog-example-text-format/rule_based', ['media_cdn/hog-example-text-format/rule_based/hog-example-text-format_model.bngl']),
+    # ],
+
 )
